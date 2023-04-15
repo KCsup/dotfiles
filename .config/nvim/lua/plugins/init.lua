8c08a6ab8714879e96ff12d273b1aa482e6a573f
KCsup <kcsup05@gmail.com>
2023-04-15 01:51:26 -0400

    Switch to lazy.nvim

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,9 @@
+return {
+   "morhetz/gruvbox",
+   { "nvim-lualine/lualine.nvim", config=true },
+   { "nvim-tree/nvim-tree.lua", config=true },
+   "nvim-tree/nvim-web-devicons",
+
+   { "windwp/nvim-autopairs", config=true },
+}
+
