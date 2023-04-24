adf579358a1923aaf21f30cc727903c85c9a0b9f
KCsup <kcsup05@gmail.com>
2023-04-24 01:00:41 -0400

    Add default nvim-cmp config.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -29,16 +29,5 @@ return {
 
     { "windwp/nvim-autopairs", config = true },
 
-    -- Completion
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-cmdline" },
-    { "hrsh7th/nvim-cmp", config = true },
-    { "L3MON4D3/LuaSnip" },
-    { "saadparwaiz1/cmp_luasnip" },
-
-    { "simrat39/rust-tools.nvim", config = true}
 }
 
