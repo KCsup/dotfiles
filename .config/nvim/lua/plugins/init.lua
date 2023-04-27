1457d5e01e85d4e2d4416e0318313c6c87265a7d
KCsup <kcsup05@gmail.com>
2023-04-26 22:47:36 -0400

    Add treesitter and telescope.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -29,5 +29,19 @@ return {
 
     { "windwp/nvim-autopairs", config = true },
 
+    { "nvim-treesitter/nvim-treesitter" },
+
+    { 'nvim-telescope/telescope.nvim',
+	tag = '0.1.1',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+	    local builtin = require('telescope.builtin')
+	    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+	    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+	    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+	    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+	end
+    },
+
 }
 
