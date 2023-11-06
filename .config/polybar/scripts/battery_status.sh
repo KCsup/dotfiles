e4c12146c164ad8a63fcca5fe50e2d0d4f18758a
KCsup <kcsup05@gmail.com>
2023-11-06 13:57:55 -0500

    Updated battery status, fixed helix config.

--- .config/polybar/scripts/battery_status.sh
+++ .config/polybar/scripts/battery_status.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-P=$(echo $(acpi -b) | cut -d "," -f 2 | cut -c 2- | tr -d ,)
-S=$(echo $(acpi -b) | cut -d "," -f 1 | cut -d ":" -f 2 | cut -c 2- | tr -d ,)
-echo "$P ($S)"
+
+ACPI_OUT=$(acpi -b)
+
+P=""
+S=""
+
+if [[ $ACPI_OUT == *"Not charging"* ]]; then
+  P=$(echo $(acpi -b) | cut -d "," -f 2 | tr -d " ")
+  S=$(echo $(acpi -b) | cut -d "," -f 1 | cut -d ":" -f 2 | tr -d " ")
+else
+  P=$(echo $(acpi -b) | cut -d "," -f 2 | cut -c 2- | tr -d ,)
+  S=$(echo $(acpi -b) | cut -d "," -f 1 | cut -d ":" -f 2 | cut -c 2- | tr -d ,)
+fi
+
+
+if [ $S = "Charging" ]; then
+  echo "$P ($S)"
+else
+  echo "$P"
+fi
